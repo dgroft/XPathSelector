4d222b34a1f17cf1a4d86753caafd0df5819041f
Dan Groft <dgroft@gmail.com>
2014-07-24 22:09:05 -0400

    first commit

---
+++ XPathSelector.py
@@ -0,0 +1,8 @@
+import sublime, sublime_plugin, re
+
+class XPathSelectorCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# TODO: pop up a window where the user enters an xpath expression
+		# evaluate the xpath expression
+		# select all instances in the buffer according to the evaluated xpath
+		# remember the last 10 xpath expressions, start with the last one
